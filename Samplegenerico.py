d2873f20b5104e7dfae88e7409af3c826cf840e9
SebaRaRe <seba_14.95@outlook.com>
2016-04-13 17:16:00 -0300

    Update Samplegenerico.py
    comentado

--- Samplegenerico.py
+++ Samplegenerico.py
@@ -1,6 +1,6 @@
-from random import randrange
+from random import randrange 
 
-def sattoloCycle(items):
+def sattoloCycle(items):            #sattoloCycle(arreglo) recibe un arreglo y lo desordena
      i = len(items)
      while i > 1:
          i = i - 1
@@ -8,15 +8,15 @@ def sattoloCycle(items):
          items[j], items[i] = items[i], items[j]
      return 
 
-cantidad = int(raw_input('Escriba un tamano para el arreglo aca: '))
+cantidad = int(raw_input('Escriba un tamano para el arreglo aca: '))  #recibimos la cantidad de elementos que queremos generar
 A=[]
 i=0
-for i in range(cantidad):
+for i in range(cantidad):    #rellenamos el arreglo 
     A.append(i)
-sattoloCycle(A)
+sattoloCycle(A)              #desordenamos el arreglo
 
-archivo_n = raw_input('Escriba un nombre para el archivo aca: ')
-archivo = open('%s.txt'% archivo_n,'w')
-for x in A:
-    archivo.write('%s \n' % A[x])
+archivo_n = raw_input('Escriba un nombre para el archivo aca: ')  #asignamos el nombre que le daremos a nuestro archivo
+archivo = open('%s.txt'% archivo_n,'w')                           #abre un archivo
+for x in A:                                                       #rellenamos el archivo con los datos del arreglo para luego cerrar
+    archivo.write('%s \n' % A[x])                                 #el archivo 
 archivo.close()
