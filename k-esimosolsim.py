752f5559c78da34054ffabf3ac5dcf520e6c9bfe
SebaRaRe <seba_14.95@outlook.com>
2016-04-13 17:11:21 -0300

    Update k-esimosolsim.py
    comente el codigo para  mejor comprension

--- k-esimosolsim.py
+++ k-esimosolsim.py
@@ -1,7 +1,6 @@
 #solucion simple de k-esimo
-import timeit
-
-def mergesort(lista):
+import timeit   #para medir tiempo
+def mergesort(lista):     #implementacion merge
     resultado = []
     if len(lista) < 2:
         return lista
@@ -26,27 +25,27 @@ def mergesort(lista):
                 izq.pop(0)
     return resultado
 
-def k_esimosimple(k,array):
-        if k == 0:
+def k_esimosimple(k,array):  
+        if k == 0:           #aca se verifica si k es cero y se imprime k es invalido
 	        print "k es invalido"
         else :
-            nuevo=mergesort(array)
-            return nuevo[k-1]
-j=raw_input('Ingresar nombre de archivo a probar sin extension(input): ')
-archivo = open('%s.txt'%j, 'r')
-A = []
-for linea in archivo:
+            nuevo=mergesort(array)   #si k no es cero, nuevo es = a la funcion merge con el arreglo y lo devuelve ordenado
+            return nuevo[k-1]        #devuelve el k menos uno porque e los arreglo tenemos la posicion 0
+j=raw_input('Ingresar nombre de archivo a probar sin extension(input): ') #con esta funcion igualamos a j una entrada de teclado
+archivo = open('%s.txt'%j, 'r')   #abre y lee el archivo ya creado        #usaremos por ejemplo "cienmil" "doscientosmil"
+A = []                            #lista vacia
+for linea in archivo:             #pasa los elemntos del archivo al arreglo vacio
 	 numero = int(linea)
 	 A.append(numero)
-archivo.close()
-print "todo va bien"
+archivo.close()                   #cerramos el archivo
+print "todo va bien"              #verificador
 
-k=int(raw_input('Ingrese el valor de k: '))
+k=int(raw_input('Ingrese el valor de k: '))    #recibimos el k por teclado y queda casteado a int
 print "todo va bien"
-a = k_esimosimple(k,A)
+a = k_esimosimple(k,A)                         # instanciamos la funcion k_esimosimple
 print "todo va bien"
 print a
-times = 1   
+times = 1                                              #estas ultimas 4 lineas deen ir para contabilizar el tiempo que tarda la funcion
 tiempo = timeit.Timer(lambda: k_esimosimple(k,A), "")
 tiempof = tiempo.repeat(times, 1)
 print tiempof
